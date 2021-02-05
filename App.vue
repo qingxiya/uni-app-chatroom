6dbe211809756cafe1959b6463f16d10c28a229b
qingxiya <qingxiya@qq.com>
2021-02-05 17:02:47 +0800

    首次提交

---
+++ App.vue
@@ -0,0 +1,22 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	
+	@import "colorui/main.css";
+	@import "colorui/icon.css";
+	@import "static/css/index-app.css";
+
+</style>
